8778f459dedb515cb024d75d4efab7572fcc7f53
w-v <wtv@protonmail.ch>
2018-04-23 18:16:20 +0200

    first test fixes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # srvsetup
 server setup script
+
+# single line run
+`wget https://raw.githubusercontent.com/w-v/srvsetup/master/srvsetup.sh;bash srvsetup.sh;rm srvsetup.sh`
--- srvsetup.sh
+++ srvsetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-TRHOME = /home/dl
-TRCONFIG = ${TRHOME}/config
-TRDOWNLOADS = ${TRHOME}/downloads
-TRWATCH = ${TRHOME}/watch
-TRUID = 101
-TRGID = 101
+TRHOME=/home/transmission
+TRCONFIG=${TRHOME}/config
+TRDOWNLOADS=${TRHOME}/downloads
+TRWATCH=${TRHOME}/watch
+TRUID=1001
+TRGID=1001
 
 # Docker installation
 
@@ -18,21 +18,25 @@ sudo apt-key add gpg
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-cache policy docker-ce
-
+sudo apt-get -y install docker-ce
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+sudo docker run hello-world
 
 # Adding transmission user
 
 echo "---------Adding transmission user----------"
 
-sudo adduser transmission --uid ${TRUID}
-sudo addgroup transmission --gid ${TRGID}
-
+sudo adduser --uid ${TRUID} --disabled-password --gecos "" transmission
+sudo addgroup --gid ${TRGID} transmission
 
 # making dirs
 
 echo "---------making dirs----------"
 
 sudo mkdir ${TRHOME} ${TRWATCH} ${TRDOWNLOADS} ${TRCONFIG} 
+sudo chown ${TRUID}:${TRGID} ${TRHOME} ${TRWATCH} ${TRDOWNLOADS} ${TRCONFIG} 
 
 
 # transmission container setup
